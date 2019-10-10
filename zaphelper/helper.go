afd87ce38f00823560725f53ee98b97887c30319
jacexh <jace.xh@gmail.com>
2019-10-10 17:32:41 +0800

    update

--- zaphelper/helper.go
+++ zaphelper/helper.go
@@ -9,41 +9,39 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-type (
-	HelperConfig struct {
-		// LoggerName 生成的Logger名称
-		LoggerName string
+type HelperConfig struct {
+	// LoggerName 生成的Logger名称
+	LoggerName string
 
-		// Filename is the file to write logs to.  Backup log files will be retained
-		// in the same directory.
-		Filename string
+	// Filename is the file to write logs to.  Backup log files will be retained
+	// in the same directory.
+	Filename string
 
-		// MaxSize is the maximum size in megabytes of the log file before it gets
-		// rotated. It defaults to 100 megabytes.
-		MaxSize int
+	// MaxSize is the maximum size in megabytes of the log file before it gets
+	// rotated. It defaults to 100 megabytes.
+	MaxSize int
 
-		// MaxAge is the maximum number of days to retain old log files based on the
-		// timestamp encoded in their filename.  Note that a day is defined as 24
-		// hours and may not exactly correspond to calendar days due to daylight
-		// savings, leap seconds, etc. The default is not to remove old log files
-		// based on age.
-		MaxAge int
+	// MaxAge is the maximum number of days to retain old log files based on the
+	// timestamp encoded in their filename.  Note that a day is defined as 24
+	// hours and may not exactly correspond to calendar days due to daylight
+	// savings, leap seconds, etc. The default is not to remove old log files
+	// based on age.
+	MaxAge int
 
-		// MaxBackups is the maximum number of old log files to retain.  The default
-		// is to retain all old log files (though MaxAge may still cause them to get
-		// deleted.)
-		MaxBackups int
+	// MaxBackups is the maximum number of old log files to retain.  The default
+	// is to retain all old log files (though MaxAge may still cause them to get
+	// deleted.)
+	MaxBackups int
 
-		// LocalTime determines if the time used for formatting the timestamps in
-		// backup files is the computer's local time.  The default is to use UTC
-		// time.
-		LocalTime bool
+	// LocalTime determines if the time used for formatting the timestamps in
+	// backup files is the computer's local time.  The default is to use UTC
+	// time.
+	LocalTime bool
 
-		// Compress determines if the rotated log files should be compressed
-		// using gzip. The default is not to perform compression.
-		Compress bool
-	}
-)
+	// Compress determines if the rotated log files should be compressed
+	// using gzip. The default is not to perform compression.
+	Compress bool
+}
 
 func buildOptionsFromConfig(cfg zap.Config, writer zapcore.WriteSyncer) []zap.Option {
 	opts := []zap.Option{zap.ErrorOutput(writer)}
